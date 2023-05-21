c8645e08bbea927194f175675644d43b6ff022ca
Jose Isaac <joseisaacbarreirocampos@gmail.com>
2023-05-21 16:12:33 -0300

    add get player profile

--- src/main/kotlin/com/workshop/battleship/resources/player/PlayerController.kt
+++ src/main/kotlin/com/workshop/battleship/resources/player/PlayerController.kt
@@ -3,13 +3,12 @@ package com.workshop.battleship.resources.player
 import com.workshop.battleship.domain.model.player.PlayerService
 import com.workshop.battleship.domain.player.toEntity
 import com.workshop.battleship.domain.player.toProfileRepresentation
+import com.workshop.battleship.infrastructure.security.utils.getCurrentPlayerInContext
 import com.workshop.battleship.resources.player.representation.PlayerProfileRepresentation
 import com.workshop.battleship.resources.player.representation.PlayerRepresentation
 import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+
 @RestController
 @RequestMapping(
     path = ["/player"],
@@ -18,9 +17,14 @@ import org.springframework.web.bind.annotation.RestController
 class PlayerController(
     private val playerService: PlayerService,
 ) {
-
     @PostMapping("/create")
     fun create(
         @RequestBody playerRepresentation: PlayerRepresentation,
     ): PlayerProfileRepresentation = playerService.createPlayer(playerRepresentation.toEntity()).toProfileRepresentation()
+
+    @GetMapping("/profile")
+    fun profile(): PlayerProfileRepresentation {
+        val player = playerService.getPlayer(getCurrentPlayerInContext())
+        return PlayerProfileRepresentation(player.username)
+    }
 }
